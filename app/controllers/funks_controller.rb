2005c9b1f5ea26566b0a1f4af24de7128bb69b5e
Patrick Racenberg <pracenberg@gmail.com>
2015-05-01 11:34:54 -0700

    added views wireframes, created methods

--- app/controllers/funks_controller.rb
+++ app/controllers/funks_controller.rb
@@ -1,2 +1,29 @@
 class FunksController < ApplicationController
+
+  def index
+    @funks = Funk.all
+  end
+
+  def view
+    @funk = Funk.find(params[:id])
+  end
+
+  def new
+    @funk = Funk.new
+  end
+
+  def create
+    @funk = Funk.create(funk_params)
+    redirect_to funks_path
+  end
+
+  def destroy
+    Funk.find(params[:id]).destroy
+  end
+
+  private
+  def funk_params
+    params.require(:funk).permit(:content)
+  end
+
 end
